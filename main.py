851d62920cc0c967b1b819630dc462798d94523c
cwavedave <david@creativewavelength.co.uk>
2021-01-06 10:04:55 +0100

    Working App

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 from tkinter import *
 from tkinter import messagebox
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+import pyperclip
+from random import randint,shuffle,choice
 
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = randint(8, 10)
+nr_symbols = randint(2, 4)
+nr_numbers = randint(2, 4)
+
+def password_generator():
+    password_entry.delete(0,END)
+    letters_list = [choice(letters) for letter in range(nr_letters)]
+    symbols_list = [choice(symbols) for symbol in range(nr_symbols)]
+    numbers_list = [choice(numbers) for number in range(nr_numbers)]
+
+    password_list = letters_list+symbols_list+numbers_list
+
+    shuffle(password_list)
+    password = "".join(password_list)
+
+    password_entry.insert(0, f"{password}")
+    pyperclip.copy(password)
+    messagebox.showinfo(title="Password saved to clipboard", message="Your password has been saved to the clipboard, ready for pasting")
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def save_password():
@@ -49,7 +73,7 @@ password_label = Label(text="Password")
 password_label.grid(column=0,row=4)
 password_entry = Entry(text="", width=21)
 password_entry.grid(column =1, row=4, columnspan=1)
-password_button = Button(text="Generate Password")
+password_button = Button(text="Generate Password", command=password_generator)
 password_button.grid(column=2, row=4)
 
 add_button = Button(text="Add", width=36, command=save_password)
