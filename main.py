dc1932818f2f8e7f94fec753d341e18b8ad988e7
cwavedave <david@creativewavelength.co.uk>
2021-01-08 23:13:25 +0100

    Working App

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 import pyperclip
 from random import randint,shuffle,choice
+import json
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -32,7 +33,15 @@ def save_password():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
+
+    print(new_data)
     if len(website) < 1 or len(email) < 1 or len(password) < 1:
         messagebox.showerror(title="Please fill out all inputs", message="You missed out on at least one field, please ensure form is filled out")
         pass
@@ -40,12 +49,59 @@ def save_password():
     elif len(password) < 1:
         messagebox.showerror(title="Password not generated", message="Please click 'Generate Password' ")
     else:
-        with open(f"./passwordlist.txt", mode="a") as password_save :
-            password_save.write(f"{website} | {email} | {password}\n")
+
+        try:
+            with open(f"./passwordlist.json", mode="r") as password_save:
+                data = json.load(password_save)
+                data.update(new_data)
+                # Reads old data
+
+        except FileNotFoundError:
+            with open(f"./passwordlist.json", mode="w") as password_save:
+                json.dump(new_data, password_save, indent=4)
+
+        else:
+            print("Something")
+            with open(f"./passwordlist.json",mode="w") as password_save:
+                json.dump(data, password_save, indent=4)
+
+        finally:
             messagebox.showinfo(title="Password Saved", message=f"Your password for {email} on {website} has been saved")
             website_entry.delete(0,END)
             password_entry.delete(0,END)
 
+# ---------------------------- FIND WEB ------------------------------- #
+
+def website_search():
+    try:
+        with open(f"./passwordlist.json", mode="r") as password_save:
+            data = json.load(password_save)
+            index = 0
+            result = ""
+            for i in data:
+                index += 1
+                if i.lower() == website_entry.get().lower():
+                    result = i
+                    break
+                else:
+                    pass
+
+        password_result = data[result]['password']
+        password_entry.delete(0, END)
+        password_entry.insert(0,f"{password_result}")
+
+    except FileNotFoundError:
+            print(f"Error - {FileNotFoundError} not found")
+            result = "Not Found"
+
+    except KeyError:
+        if website_entry.get() == '':
+            print("No Website Entered ")
+        else:
+            messagebox.showinfo(title="Not Found", message=f"No Website found for '{website_entry.get()}'")
+
+    finally:
+        print("Finished")
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -60,9 +116,11 @@ canvas.grid(column=1,row=1)
 
 website_label = Label(text="Website")
 website_label.grid(column=0,row=2)
-website_entry = Entry(text="", width=35)
+website_entry = Entry(text="", width=21)
 website_entry.focus()
-website_entry.grid(column =1, row=2, columnspan=2)
+website_entry.grid(column =1, row=2, columnspan=1)
+website_button = Button(text="Search", command=website_search)
+website_button.grid(column=2,row=2)
 
 email_label = Label(text="Email / Username")
 email_label.grid(column=0,row=3)
